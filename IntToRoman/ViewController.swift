2e12381447a892b9fb3ddb8d759a3b497c1f35d4
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-09-11 23:10:00 +0300

    Start do logic

--- IntToRoman/ViewController.swift
+++ IntToRoman/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterNumberTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var convertButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        convertButton.layer.borderWidth = 150
+        convertButton.layer.cornerRadius = 7
     }
 
-
+    @IBAction func convertButtonPressed() {
+//        if let number = (enterNumberTF.text != nil) {
+//            
+//        }
+//        guard Int(enterNumberTF.text) > 0 || Int(enterNumberTF.text) < 4000 else {
+//            <#statements#>
+//        }
+        resultLabel.text = enterNumberTF.text
+    }
+    
 }
 

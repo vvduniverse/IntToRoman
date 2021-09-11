d950fd8fdf4bf3cece6856fd64fef631d8bd2a6d
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-09-11 23:43:16 +0300

    Conditions improvements

--- IntToRoman/ViewController.swift
+++ IntToRoman/ViewController.swift
@@ -8,34 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var enterNumberTF: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var convertButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-//        convertButton.layer.borderWidth = 150
         convertButton.layer.cornerRadius = 7
     }
-
-    @IBAction func convertButtonPressed() {
-        if let text = enterNumberTF.text {
-            if let number = Int(text) {
-                if number > 0 || number < 4000 {
-                    
-                    resultLabel.text = enterNumberTF.text
-                } else {
-                    print("ERROR")
-                }
-                
-            }
-
-        }
-//        guard Int(enterNumberTF.text) > 0 || Int(enterNumberTF.text) < 4000 else {
-//            <#statements#>
-//        }
-        
-    }
     
+    @IBAction func convertButtonPressed() {
+        if let text = enterNumberTF.text, let number = Int(text), number > 0 && number < 4000 {
+            resultLabel.text = enterNumberTF.text
+        } else {
+            print("ERROR")
+            
+        }
+    }
 }
 
